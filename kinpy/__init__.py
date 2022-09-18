b836332cafb5dcb78d6660d99065174bbeabe0d7
nekanat <nekanat.stock@gmail.com>
2022-09-18 10:51:28 +0900

    add build chain from filename

--- kinpy/__init__.py
+++ kinpy/__init__.py
@@ -1,5 +1,19 @@
+import os
 from .mjcf import *
 from .sdf import *
 from .transform import *
 from .urdf import *
 from .visualizer import *
+from .chain import Chain
+
+
+def build_chain_from_file(filename: str) -> Chain:
+    ext = os.path.splitext(filename)[-1]
+    if ext == ".urdf":
+        return build_chain_from_urdf(open(filename).read())
+    elif ext == ".sdf":
+        return build_chain_from_sdf(open(filename).read())
+    elif ext == ".mjcf":
+        return build_chain_from_mjcf(open(filename).read())
+    else:
+        raise ValueError(f"Invalid file type: '{ext}' file.")

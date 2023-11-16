9ede99cfaf638805b2cebd7a8c30610c014c6bb4
neka-nat <nekanat.stock@gmail.com>
2023-11-16 22:36:59 +0900

    pump head

--- examples/mycobot_example.py
+++ examples/mycobot_example.py
@@ -1,7 +1,7 @@
 import numpy as np
 import kinpy as kp
 
-chain = kp.build_serial_chain_from_urdf(open("mycobot/mycobot.urdf").read(), "joint6_flange")
+chain = kp.build_serial_chain_from_urdf(open("mycobot/mycobot.urdf").read(), "pump_head")
 print(chain)
 
 print(chain.get_joint_parameter_names())
